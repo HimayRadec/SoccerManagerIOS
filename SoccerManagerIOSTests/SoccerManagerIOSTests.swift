0b9806edd18287f3c89f99e322e6cf283dc42de3
HimayRadec <himayradec@gmail.com>
2025-01-20 03:07:57 -0700

    Initial Commit

---
+++ SoccerManagerIOSTests/SoccerManagerIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerManagerIOSTests.swift
+//  SoccerManagerIOSTests
+//
+//  Created by Himay on 1/20/25.
+//
+
+import Testing
+@testable import SoccerManagerIOS
+
+struct SoccerManagerIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
